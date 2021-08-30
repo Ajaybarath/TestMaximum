a9772ce11a50859b79da1e775e1010240487b15b
ajaybarath <ajaybarath3@gmail.com>
2021-08-30 15:26:45 +0530

    REFACTORED: generics method to find maximum

--- src/com/bridgeLabz/testMaximum/TestMaximum.java
+++ src/com/bridgeLabz/testMaximum/TestMaximum.java
@@ -8,9 +8,9 @@ public class TestMaximum {
 		Integer integerTestCase2[] = {4,5,3};
 		Integer integerTestCase3[] = {4,2,9};
 		
-		float floatTestCase1[] = {4,2,3};
-		float floatTestCase2[] = {4,5,3};
-		float floatTestCase3[] = {4,2,9};
+		Float floatTestCase1[] = {4f,2f,3f};
+		Float floatTestCase2[] = {4f,5f,3f};
+		Float floatTestCase3[] = {4f,2f,9f};
 		
 		String stringTestCase1[] = { "peach", "apple", "banana"};
 		String stringTestCase2[] = {"apple", "peach", "banana"};
@@ -18,23 +18,23 @@ public class TestMaximum {
 
 
 		
-		findMaximumInteger(integerTestCase1);
-		findMaximumInteger(integerTestCase2);
-		findMaximumInteger(integerTestCase3);
+		findMaximum(integerTestCase1);
+		findMaximum(integerTestCase2);
+		findMaximum(integerTestCase3);
 		
-		findMaximumFloat(floatTestCase1);
-		findMaximumFloat(floatTestCase2);
-		findMaximumFloat(floatTestCase3);
+		findMaximum(floatTestCase1);
+		findMaximum(floatTestCase2);
+		findMaximum(floatTestCase3);
 		
-		findMaximumString(stringTestCase1);
-		findMaximumString(stringTestCase2);
-		findMaximumString(stringTestCase3);
+		findMaximum(stringTestCase1);
+		findMaximum(stringTestCase2);
+		findMaximum(stringTestCase3);
 
 	}
 
-	private static void findMaximumInteger(Integer testCase[]) {
+	private static <T extends Comparable<T>> void findMaximum(T testCase[]) {
 
-		Integer maximum = 0;
+		T maximum = testCase[0];
 		
 		for (int i=0;i<3;i++) {
 			if (maximum.compareTo(testCase[i]) < 0) {
@@ -45,33 +45,5 @@ public class TestMaximum {
 		System.out.println("The maximum is " + maximum);
 		
 	}
-	
-	private static void findMaximumFloat(float testCase[]) {
-
-		Float maximum = (float) 2;
 		
-		for (int i=0;i<3;i++) {
-			if (maximum.compareTo(testCase[i]) < 0) {
-				maximum = testCase[i];
-			}
-		}
-		
-		System.out.println("The maximum is " + maximum);
-		
-	}
-	
-	private static void findMaximumString(String testCase[]) {
-
-		String maximum = "";
-		
-		for (int i=0;i<3;i++) {
-			if (maximum.compareTo(testCase[i]) < 0) {
-				maximum = testCase[i];
-			}
-		}
-		
-		System.out.println("The maximum is " + maximum);
-		
-	}
-	
 }
